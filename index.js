05d135004073de3c81c54e802a143f4ec738df12
Rob Merrill <robgmerrill@gmail.com>
2016-02-20 13:49:50 -0800

    Rest API pushes

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-var mongoose = require('mongoose');
-var schema = require('./schema');
+var express = require('express');
+var wagner = require('wagner-core');
 
-mongoose.connect('mongodb://localhost:27017/test');
+require('./models')(wagner);
+require('./dependencies')(wagner);
 
-var User = mongoose.model('User', schema, 'users');
+var app = express();
 
-var user = new User({
-  name: 'John Smith',
-  email: 'john@smith.io'
-});
+wagner.invoke(require('./auth'), { app: app });
 
-user.save(function(err) {
-  if(err) {
-    console.log(error);
-    process.exit(1);
-  }
-  User.find({email: 'john@smith.io'}, function(err, docs) {
-    if(err) {
-      console.log(err);
-      process.exit(1);
-    }
-    console.log(require('util').inspect(docs));
-    process.exit(0);
-  });
-});
+app.use('/api/v1', require('./api')(wagner));
+
+app.listen(3000);
+console.log('Listening on port 3000!');
+
+
+
+
+
+// var mongoose = require('mongoose');
+// var schema = require('./schema');
+
+// mongoose.connect('mongodb://localhost:27017/test');
+
+// var User = mongoose.model('User', schema, 'users');
+
+// var user = new User({
+//   name: 'John Smith',
+//   email: 'john@smith.io'
+// });
+
+// user.save(function(err) {
+//   if(err) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+//   User.find({email: 'john@smith.io'}, function(err, docs) {
+//     if(err) {
+//       console.log(err);
+//       process.exit(1);
+//     }
+//     console.log(require('util').inspect(docs));
+//     process.exit(0);
+//   });
+// });
