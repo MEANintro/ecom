9543f43ef0f66d6813fe5e2ecf4c0de5d6becbe2
jacethelefty <jacemontoya@live.com>
2016-02-20 09:59:50 -0800

    Js mongoose push

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-var fn = require('./myfile.js');
-fn();
+var mongoose = require('mongoose');
+var schema = require('./schema');
 
-var otherFn = require('./test').other;
-otherFn();
+mongoose.connect('mongodb://localhost:27017/test');
+
+var User = mongoose.model('User', schema, 'users');
+
+var user = new User({
+  name: 'John Smith',
+  email: 'john@smith.io'
+});
+
+user.save(function(err) {
+  if(err) {
+    console.log(error);
+    process.exit(1);
+  }
+  User.find({email: 'john@smith.io'}, function(err, docs) {
+    if(err) {
+      console.log(err);
+      process.exit(1);
+    }
+    console.log(require('util').inspect(docs));
+    process.exit(0);
+  });
+});
